7bbc018dfda5f339696490dc219f561603237cde
Cryo79 <johannes2503@gmail.com>
2023-02-12 14:15:50 +0000

    added profile permissions functionality

--- profiles/views.py
+++ profiles/views.py
@@ -4,34 +4,37 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-
+from joda_movies_api.permissions import IsUserOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
         return Response(serializer.data)
 
 
 
 class ProfileGetDetails(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsUserOrReadOnly]
+
     def get_details(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
         
     def get(self, request, pk):
         profile = self.get_details(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_details(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
