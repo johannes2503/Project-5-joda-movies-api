61fbcca83d1a8878b6134f9b714b213c6d3eaab0
Cryo79 <johannes2503@gmail.com>
2023-02-12 17:38:13 +0000

    added a model to ratings app

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Rating(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    
