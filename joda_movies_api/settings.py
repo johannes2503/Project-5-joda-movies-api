ab4a4114321635400bd9dce8a0fcf2bbcde5525c
Cryo79 <johannes2503@gmail.com>
2023-02-12 12:19:27 +0000

    Adding serialziser function and profiles

--- joda_movies_api/settings.py
+++ joda_movies_api/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
+    "rest_framework",
     "profiles",
 ]
 
