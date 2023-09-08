db630a7e00707d27d069ae836cd8148930c17739
Cryo79 <johannes2503@gmail.com>
2023-09-08 21:43:19 +0000

    Deploying to heroku - fixing deploy bug

--- joda_movies/settings.py
+++ joda_movies/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'joda-movies-api-bfcd089e7a9c.herokuapp.com/']
 
